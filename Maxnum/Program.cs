166a4450500219e637d52285c37f74301b9bb599
Tata Venkata Datta Sai Sreyanth <2100080197ai.ds@gmail.com>
2026-04-17 11:09:23 +0530

    init

---
+++ Maxnum/Program.cs
@@ -0,0 +1,12 @@
+﻿int[] a = {10, 25, 15};
+int l= a[0];
+for  (int i = 1;i<a.Length; i++)
+{
+    if (a[i] > l)
+    {
+        l = a[i];
+    }
+    Console.WriteLine(l);
+    break;
+}
+Console.WriteLine(a.Max());

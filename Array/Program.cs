2216652a4fc014a5a2a9cf8391619f953a8087ef
Tata Venkata Datta Sai Sreyanth <2100080197ai.ds@gmail.com>
2026-04-17 10:52:57 +0530

    init

---
+++ Array/Program.cs
@@ -0,0 +1,13 @@
+﻿//create a array
+string[] cars = { "volvo", "BMW", "Ford" };
+int[] num = { 1, 2, 3, 58, 4 };
+Console.WriteLine(num.Length);
+for(int i=0; i<num.Length; i++)
+{
+    Console.WriteLine(num[i]);
+}
+Console.WriteLine("\nString values:");
+for(int j=0; j<cars.Length; j++)
+{
+    Console.WriteLine(cars[j]);
+}
